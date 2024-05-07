6b76883405663369a105fbb56839e873b902d516
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-05-07 02:26:03 +0100

    pubspec updated

--- lib/screens/onboard_screen/onboard_screen.dart
+++ lib/screens/onboard_screen/onboard_screen.dart
@@ -5,8 +5,13 @@ class OnBoardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 1, 32, 72),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 1, 32, 72),
+      body: Column(
+        children: [
+          Image.asset('assets/images/iphone.png'),
+        ],
+      ),
     );
   }
 }
