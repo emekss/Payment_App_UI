d19e57150dd750531eb27f1113899dfe16bdcb62
Chukwuemeka Kennedy <princeukaga@gmail.com>
2024-05-12 23:02:24 +0100

    onboard screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:payment_app_ui/screens/home_screen/home_screen.dart';
+import 'package:payment_app_ui/screens/onboard_screen/onboard_screen.dart';
 import 'package:payment_app_ui/screens/wallet_page/wallet_page.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: WalletScreen(),
+      home: OnBoardScreen(),
     );
   }
 }
--- lib/screens/onboard_screen/onboard_screen.dart
+++ lib/screens/onboard_screen/onboard_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:payment_app_ui/screens/onboard_screen/components/user_login_container.dart';
 
 class OnBoardScreen extends StatelessWidget {
@@ -11,12 +9,14 @@ class OnBoardScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 1, 32, 72),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Stack(
             children: [
               Image.asset('assets/images/iphone.png'),
-              const UserLoginContainer(),
+              const Positioned(
+                child: UserLoginContainer(),
+              ),
             ],
           ),
         ],
